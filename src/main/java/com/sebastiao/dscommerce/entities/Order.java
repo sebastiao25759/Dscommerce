40f490706d61074c2cf46bfc47f76c6649cb0382
sebastiao25759 <sebastiao.rodrigues.25759@aluno.facape.br>
2025-12-14 16:51:16 -0300

    OrderItem and OrderItemPK class

--- src/main/java/com/sebastiao/dscommerce/entities/Order.java
+++ src/main/java/com/sebastiao/dscommerce/entities/Order.java
@@ -3,7 +3,10 @@ package com.sebastiao.dscommerce.entities;
 import jakarta.persistence.*;
 
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_order")
@@ -23,6 +26,9 @@ public class Order {
 
     @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
     private Payment payment;
+
+    @OneToMany(mappedBy = "id.order")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Order() {
     }
@@ -73,6 +79,10 @@ public class Order {
 
     public void setPayment(Payment payment) {
         this.payment = payment;
+    }
+
+    public Set<OrderItem> getItems() {
+        return items;
     }
 
     @Override
--- src/main/java/com/sebastiao/dscommerce/entities/Product.java
+++ src/main/java/com/sebastiao/dscommerce/entities/Product.java
@@ -25,6 +25,9 @@ public class Product {
             inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
 
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
+
     public Product() {
     }
 
@@ -78,6 +81,10 @@ public class Product {
 
     public Set<Category> getCategories() {
         return categories;
+    }
+
+    public Set<OrderItem> getItems() {
+        return items;
     }
 
     @Override
