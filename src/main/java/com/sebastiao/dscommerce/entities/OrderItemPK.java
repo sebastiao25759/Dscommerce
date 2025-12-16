40e0327fc4c255e7a9205ebde482e9715dc59d99
sebastiao25759 <sebastiao.rodrigues.25759@aluno.facape.br>
2025-12-16 17:58:41 -0300

    refactor: implement equals and hashCode in OrderItem and OrderItemPK

--- src/main/java/com/sebastiao/dscommerce/entities/OrderItemPK.java
+++ src/main/java/com/sebastiao/dscommerce/entities/OrderItemPK.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Embeddable;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 
+import java.util.Objects;
+
 @Embeddable
 public class OrderItemPK {
 
@@ -37,5 +39,16 @@ public class OrderItemPK {
 
     public void setProduct(Product product) {
         this.product = product;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof OrderItemPK that)) return false;
+        return Objects.equals(order, that.order) && Objects.equals(product, that.product);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(order, product);
     }
 }
