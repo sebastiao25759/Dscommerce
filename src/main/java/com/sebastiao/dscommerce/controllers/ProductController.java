9a1de975dab7b1721e3c9bb90d2230dd9bb5a0a0
sebastiao25759 <sebastiao.rodrigues.25759@aluno.facape.br>
2025-12-25 19:44:23 -0300

    GET product by id

--- src/main/java/com/sebastiao/dscommerce/controllers/ProductController.java
+++ src/main/java/com/sebastiao/dscommerce/controllers/ProductController.java
@@ -1,9 +1,12 @@
 package com.sebastiao.dscommerce.controllers;
 
+import com.sebastiao.dscommerce.dto.ProductDTO;
 import com.sebastiao.dscommerce.entities.Product;
 import com.sebastiao.dscommerce.repositories.ProductRepository;
+import com.sebastiao.dscommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,12 +17,10 @@ import java.util.Optional;
 public class ProductController {
 
     @Autowired
-    private ProductRepository repository;
+    private ProductService service;
 
-    @GetMapping
-    public String findAll() {
-        Optional<Product> result = repository.findById(1L);
-        Product product = result.get();
-        return product.getName();
+    @GetMapping(value = "/{id}")
+    public ProductDTO findById(@PathVariable Long id) {
+        return service.findById(id);
     }
 }
---
+++ src/main/java/com/sebastiao/dscommerce/dto/ProductDTO.java
@@ -0,0 +1,51 @@
+package com.sebastiao.dscommerce.dto;
+
+import com.sebastiao.dscommerce.entities.Product;
+
+public class ProductDTO {
+
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+    private String imgUrl;
+
+    public ProductDTO() {
+    }
+
+    public ProductDTO(Long id, String name, String description, Double price, String imgUrl) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.imgUrl = imgUrl;
+    }
+
+    public ProductDTO(Product obj) {
+        id = obj.getId();
+        name = obj.getName();
+        description = obj.getDescription();
+        price = obj.getPrice();
+        imgUrl = obj.getImgUrl();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+}
