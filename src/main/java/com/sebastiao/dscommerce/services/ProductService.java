3119cd8abde951fba083e411117c293abffd098d
sebastiao25759 <sebastiao.rodrigues.25759@aluno.facape.br>
2025-12-26 16:40:00 -0300

    GET Products paged

--- src/main/java/com/sebastiao/dscommerce/services/ProductService.java
+++ src/main/java/com/sebastiao/dscommerce/services/ProductService.java
@@ -4,9 +4,12 @@ import com.sebastiao.dscommerce.dto.ProductDTO;
 import com.sebastiao.dscommerce.entities.Product;
 import com.sebastiao.dscommerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -14,6 +17,12 @@ public class ProductService {
 
     @Autowired
     private ProductRepository repository;
+
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable){
+        Page<Product> result = repository.findAll(pageable);
+        return result.map(ProductDTO::new);
+    }
 
     @Transactional(readOnly = true)
     public ProductDTO findById(Long id) {
