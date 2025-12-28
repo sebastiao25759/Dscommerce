cfbf4df82045f969db1ff494124ad5f99e13d942
sebastiao25759 <sebastiao.rodrigues.25759@aluno.facape.br>
2025-12-28 16:38:13 -0300

    Add Validation

--- src/main/java/com/sebastiao/dscommerce/dto/ProductDTO.java
+++ src/main/java/com/sebastiao/dscommerce/dto/ProductDTO.java
@@ -1,12 +1,20 @@
 package com.sebastiao.dscommerce.dto;
 
 import com.sebastiao.dscommerce.entities.Product;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 
 public class ProductDTO {
 
     private Long id;
+    @Size(min = 3,max = 80, message = "Nome precisa ter de 3 a 80 caracteres")
+    @NotBlank(message = "Campo Obrigatório")
     private String name;
+    @Size(min = 10, message = "Descrição precisa ter no minimo 10 caracteres")
+    @NotBlank(message = "Campo Obrigatório")
     private String description;
+    @Positive(message = "O preço deve ser positivo")
     private Double price;
     private String imgUrl;
 
