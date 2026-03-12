dac568b04bfa3dafea3b68a59f817dccf69f58f9
Sebastião <sebastianbach4212@gmail.com>
2026-03-11 23:39:35 -0300

    Fix validation

--- src/main/java/com/sebastiao/dscommerce/dto/ProductDTO.java
+++ src/main/java/com/sebastiao/dscommerce/dto/ProductDTO.java
@@ -2,10 +2,7 @@ package com.sebastiao.dscommerce.dto;
 
 import com.sebastiao.dscommerce.entities.Category;
 import com.sebastiao.dscommerce.entities.Product;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,12 +10,13 @@ import java.util.List;
 public class ProductDTO {
 
     private Long id;
-    @Size(min = 3,max = 80, message = "Nome precisa ter de 3 a 80 caracteres")
     @NotBlank(message = "Campo Obrigatório")
+    @Size(min = 3,max = 80, message = "Nome precisa ter de 3 a 80 caracteres")
     private String name;
     @Size(min = 10, message = "Descrição precisa ter no minimo 10 caracteres")
     @NotBlank(message = "Campo Obrigatório")
     private String description;
+    @NotNull(message = "Campo requerido")
     @Positive(message = "O preço deve ser positivo")
     private Double price;
     private String imgUrl;
